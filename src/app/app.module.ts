3822278ddcd3f68527ff514dd8ca7c09c571be63
Lucas dos Santos <lucas.rfl.santos.94@gmail.com>
2019-07-23 18:33:48 -0300

    commit ajuste sign facebook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { JukeboxHomeComponent } from './jukebox-home/jukebox-home.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AppComponent} from './app.component';
+import {SocialLoginModule, AuthServiceConfig} from 'angularx-social-login';
+import {GoogleLoginProvider, FacebookLoginProvider} from 'angularx-social-login';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {JukeboxHomeComponent} from './jukebox-home/jukebox-home.component';
 import {DynamicScriptLoaderService} from './services/dynamicScriptLoader/dynamic-script-loader.service';
 
 const routes: Routes = [
-  { path: 'login', component: LoginPageComponent},
-  { path: '', component: LoginPageComponent},
-  { path: '', component: LoginPageComponent},
-  { path: 'jukebox', component: JukeboxHomeComponent}];
+  {path: 'login', component: LoginPageComponent},
+  {path: '', component: LoginPageComponent},
+  {path: '', component: LoginPageComponent},
+  {path: 'jukebox', component: JukeboxHomeComponent}];
+
+const config = new AuthServiceConfig([
+  // {
+  //   id: GoogleLoginProvider.PROVIDER_ID,
+  //   provider: new GoogleLoginProvider("Google-OAuth-Client-Id")
+  // },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider('1030532580488866')
+  }
+]);
+
+
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [
@@ -19,11 +37,19 @@ const routes: Routes = [
     JukeboxHomeComponent
   ],
   imports: [
+    SocialLoginModule,
     RouterModule.forRoot(routes),
     BrowserModule
   ],
   exports: [RouterModule],
-  providers: [DynamicScriptLoaderService],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    },
+    DynamicScriptLoaderService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 
+import { AuthService } from 'angularx-social-login';
+import { FacebookLoginProvider, GoogleLoginProvider } from 'angularx-social-login';
+import { SocialUser } from 'angularx-social-login';
+
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -8,12 +13,26 @@ import {Router} from '@angular/router';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  private user: SocialUser;
+  private loggedIn: boolean;
+
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
   }
 
   login() {
+    this.router.navigate(['jukebox']);
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+    console.log(this.user.email);
+    console.log(this.user.name);
     this.router.navigate(['jukebox']);
   }
 }
